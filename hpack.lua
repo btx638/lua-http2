0ae1cdda0d26de0d0f8769b2cbc9b2d81df1132b
Murillo Rodrigues de Paula <murdrof@gmail.com>
2018-05-07 05:11:00 -0300

    hpack: encodes a header field found in the static table

---
+++ hpack.lua
@@ -0,0 +1,119 @@
+local hpack = {}
+local static_table = {}
+local dynamic_table = {}
+local fd = io.open("dump.bin", "w")
+
+local function add_static_table(i, name, value)
+  local header_field = string.pack("s4s4", name, value or "")
+  static_table[header_field] = i
+  static_table[i] = header_field
+end
+
+local function encode_integer(i, prefix, mask)
+  assert(i >= 0 and i % 1 == 0)
+  assert(prefix >= 0 and prefix <= 8 and prefix % 1 == 0)
+  assert(mask >= 0 and mask <= 256 and mask % 1 == 0)
+  if i < 2^prefix then
+    return string.char(mask | i)
+  else
+    local prefix_mask = 2^prefix-1
+    local chars = {
+      prefix_mask | mask;
+    }
+    local j = 2
+    i = i - prefix_mask
+    while i >= 128 do
+      chars[j] = i % 128 + 128
+      j = j + 1
+      i = math.floor(i / 128)
+    end
+    chars[j] = i
+    return string.char(table.unpack(chars, 1, j))
+  end
+end
+
+local function add(header_field, huffman)
+  local i = static_table[header_field]
+  if i then return encode_integer(i, 7, 0x80) end
+end
+
+function hpack.serialize(header_list)
+  local header_block = {}
+  for name, value in pairs(header_list) do
+    header_field = string.pack("s4s4", name, value or "")
+    table.insert(header_block, add(header_field, huffman))
+  end
+  return header_block
+end
+
+function hpack.decode(fragment)
+end
+
+add_static_table( 1, ":authority")
+add_static_table( 2, ":method", "GET")
+add_static_table( 3, ":method", "POST")
+add_static_table( 4, ":path", "/")
+add_static_table( 5, ":path", "/index.html")
+add_static_table( 6, ":scheme", "http")
+add_static_table( 7, ":scheme", "https")
+add_static_table( 8, ":status", "200")
+add_static_table( 9, ":status", "204")
+add_static_table(10, ":status", "206")
+add_static_table(11, ":status", "304")
+add_static_table(12, ":status", "400")
+add_static_table(13, ":status", "404")
+add_static_table(14, ":status", "500")
+add_static_table(15, "accept-charset")
+add_static_table(16, "accept-encoding", "gzip, deflate")
+add_static_table(17, "accept-language")
+add_static_table(18, "accept-ranges")
+add_static_table(19, "accept")
+add_static_table(20, "access-control-allow-origin")
+add_static_table(21, "age")
+add_static_table(22, "allow")
+add_static_table(23, "authorization")
+add_static_table(24, "cache-control")
+add_static_table(25, "content-disposition")
+add_static_table(26, "content-encoding")
+add_static_table(27, "content-language")
+add_static_table(28, "content-length")
+add_static_table(29, "content-location")
+add_static_table(30, "content-range")
+add_static_table(31, "content-type")
+add_static_table(32, "cookie")
+add_static_table(33, "date")
+add_static_table(34, "etag")
+add_static_table(35, "expect")
+add_static_table(36, "expires")
+add_static_table(37, "from")
+add_static_table(38, "host")
+add_static_table(39, "if-match")
+add_static_table(40, "if-modified-since")
+add_static_table(41, "if-none-match")
+add_static_table(42, "if-range")
+add_static_table(43, "if-unmodified-since")
+add_static_table(44, "last-modified")
+add_static_table(45, "link")
+add_static_table(46, "location")
+add_static_table(47, "max-forwards")
+add_static_table(48, "proxy-authenticate")
+add_static_table(49, "proxy-authorization")
+add_static_table(50, "range")
+add_static_table(51, "referer")
+add_static_table(52, "refresh")
+add_static_table(53, "retry-after")
+add_static_table(54, "server")
+add_static_table(55, "set-cookie")
+add_static_table(56, "strict-transport-security")
+add_static_table(57, "transfer-encoding")
+add_static_table(58, "user-agent")
+add_static_table(59, "vary")
+add_static_table(60, "via")
+add_static_table(61, "www-authenticate")
+
+local header_block = hpack.serialize({[":method"] = "GET"})
+for k, v in pairs(header_block) do fd:write(v) end
+--local header_list = hpack.decode(header_block)
+--for k, v in pairs(header_list) do print(k, v) end
+
+return hpack
