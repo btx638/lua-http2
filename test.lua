d4a0378bfb1cec730afc2cd3a657b87c38d38c53
Murillo Rodrigues de Paula <murdrof@gmail.com>
2018-05-10 04:37:01 -0300

    test: bigger test case

--- test.lua
+++ test.lua
@@ -2,7 +2,13 @@ local fd = io.open("dump.bin", "w")
 local hpack = require "hpack"
 local encoding_context = hpack.new(4096)
 --local header_block = hpack.encode(encoding_context, {[":method"] = "GET"})
-local header_block = hpack.encode(encoding_context, {[1] = {["custom-key"] = "custom-header"}, [2] = {[":path"] = "/sample/path"}})
+local header_block = hpack.encode(encoding_context, ({[1] = {[":method"] = "GET"},
+                                                      [2] = {[":path"] = "/"},
+                                                      [3] = {[":scheme"] = "http"},
+                                                      [4] = {[":authority"] = "localhost:5000"},
+                                                      [5] = {["accept"] = "*/*"},
+                                                      [6] = {["user-agent"] = "http2_client"}
+                                                     }))
 --local header_block = hpack.encode(encoding_context, {[1] = {[":path"] = "/sample/path"}})
 fd:write(header_block)
 local header_list = hpack.decode(encoding_context, header_block)
