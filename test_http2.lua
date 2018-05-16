1d35666828aae3b37401fbe1b8361468567083fc
Murillo Rodrigues de Paula <murdrof@gmail.com>
2018-05-15 21:38:08 -0300

    test_http2: now passing a body to the request

--- test_http2.lua
+++ test_http2.lua
@@ -1,2 +1,3 @@
 local http2 = require "http2"
-http2.request("localhost")
+local request_body = "<html><head><title>ko</title></head><body><h1>KO</h1><hr><address>nghttpd nghttp2/1.30.0 at port 8080</address></body></html>"
+http2.request("localhost", request_body)
