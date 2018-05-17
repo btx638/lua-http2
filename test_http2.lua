55c93208c70a9c5cdf4debedafee7e268c2dcc2a
Murillo Rodrigues de Paula <murdrof@gmail.com>
2018-05-17 04:30:12 -0300

    test_http2: GET request as default

--- test_http2.lua
+++ test_http2.lua
@@ -1,3 +1,3 @@
 local http2 = require "http2"
 local request_body = "<html><head><title>ko</title></head><body><h1>KO</h1><hr><address>nghttpd nghttp2/1.30.0 at port 8080</address></body></html>"
-http2.request("localhost", request_body)
+http2.request("localhost")
