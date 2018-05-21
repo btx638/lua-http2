583b757621f3deb10118a3f3379010de8fc0f6be
Murillo Rodrigues de Paula <murdrof@gmail.com>
2018-05-21 04:44:02 -0300

    test_http2: making the change to GET/POST tests a bit easier

--- test_http2.lua
+++ test_http2.lua
@@ -1,6 +1,6 @@
 local http2 = require "http2"
 local request_body = "<html><head><title>ko</title></head><body><h1>KO</h1><hr><address>nghttpd nghttp2/1.30.0 at port 8080</address></body></html>"
-http2.request("localhost", [[
+http2.request("localhost") --[[
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -514,5 +514,5 @@ http2.request("localhost", [[
 
   </body>
 </html>
-]]
-)
+]]--
+--)
