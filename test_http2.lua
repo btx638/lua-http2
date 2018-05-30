c2b0a657b7e9b7c5f1ddc8788a32ec6b7bacf84e
Murillo Rodrigues de Paula <murdrof@gmail.com>
2018-05-30 05:42:30 -0300

    test_http2: testing stream multiplexing in the new API

--- test_http2.lua
+++ test_http2.lua
@@ -1,3 +1,19 @@
 local http2 = require "http2"
-local body = http2.request("localhost", 8080)
-print(body)
+
+local headers = {{[1] = {[":method"] = "GET"},
+                  [2] = {[":path"] = "/"},
+                  [3] = {[":scheme"] = "http"},
+                  [4] = {[":authority"] = "localhost:8080"},
+                 },
+                 {[1] = {[":method"] = "GET"},
+                  [2] = {[":path"] = "/index2.html"},
+                  [3] = {[":scheme"] = "http"},
+                  [4] = {[":authority"] = "localhost:8080"},
+                 },
+                 {[1] = {[":method"] = "GET"},
+                  [2] = {[":path"] = "/index3.html"},
+                  [3] = {[":scheme"] = "http"},
+                  [4] = {[":authority"] = "localhost:8080"},
+                }}
+
+local body = http2.request("localhost", 8080, nil, headers)
