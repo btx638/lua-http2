a5f8e64a7e0cf1c39bead3d55b967cff548e8520
Murillo Rodrigues de Paula <murdrof@gmail.com>
2018-05-30 06:59:01 -0300

    test_http2: attempt to make header construction more readable

--- test_http2.lua
+++ test_http2.lua
@@ -1,19 +1,26 @@
 local http2 = require "http2"
 
-local headers = {{[1] = {[":method"] = "GET"},
-                  [2] = {[":path"] = "/"},
-                  [3] = {[":scheme"] = "http"},
-                  [4] = {[":authority"] = "localhost:8080"},
-                 },
-                 {[1] = {[":method"] = "GET"},
-                  [2] = {[":path"] = "/index2.html"},
-                  [3] = {[":scheme"] = "http"},
-                  [4] = {[":authority"] = "localhost:8080"},
-                 },
-                 {[1] = {[":method"] = "GET"},
-                  [2] = {[":path"] = "/index3.html"},
-                  [3] = {[":scheme"] = "http"},
-                  [4] = {[":authority"] = "localhost:8080"},
-                }}
+local h1 = {}
+table.insert(h1, {[":method"] = "GET"})
+table.insert(h1, {[":path"] = "/"})
+table.insert(h1, {[":scheme"] = "http"})
+table.insert(h1, {[":authority"] = "localhost:8080"})
 
-local body = http2.request("localhost", 8080, nil, headers)
+local h2 = {}
+table.insert(h2, {[":method"] = "GET"})
+table.insert(h2, {[":path"] = "/index2.html"})
+table.insert(h2, {[":scheme"] = "http"})
+table.insert(h2, {[":authority"] = "localhost:8080"})
+
+local h3 = {}
+table.insert(h3, {[":method"] = "GET"})
+table.insert(h3, {[":path"] = "/index3.html"})
+table.insert(h3, {[":scheme"] = "http"})
+table.insert(h3, {[":authority"] = "localhost:8080"})
+
+local headers = {}
+table.insert(headers, h1)
+table.insert(headers, h2)
+table.insert(headers, h3)
+
+local b1, b2, b3 = http2.request("localhost", 8080, nil, headers)
