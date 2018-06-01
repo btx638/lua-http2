f433ba41377278375d6e24613602bb347d7a162d
Murillo Rodrigues de Paula <murdrof@gmail.com>
2018-06-01 01:27:47 -0300

    test_request: sends one request

--- test_request.lua
+++ test_request.lua
@@ -1,26 +1,14 @@
 local http2 = require "http2"
 local copas = require "copas"
 
-local h1 = {}
-table.insert(h1, {[":method"] = "GET"})
-table.insert(h1, {[":path"] = "/"})
-table.insert(h1, {[":scheme"] = "http"})
-table.insert(h1, {[":authority"] = "localhost:8080"})
+local headers = {}
+table.insert(headers, {[":method"] = "GET"})
+table.insert(headers, {[":path"] = "/"})
+table.insert(headers, {[":scheme"] = "http"})
+table.insert(headers, {[":authority"] = "localhost:8080"})
 
-local h2 = {}
-table.insert(h2, {[":method"] = "GET"})
-table.insert(h2, {[":path"] = "/index2.html"})
-table.insert(h2, {[":scheme"] = "http"})
-table.insert(h2, {[":authority"] = "localhost:8080"})
-
-local h3 = {}
-table.insert(h3, {[":method"] = "GET"})
-table.insert(h3, {[":path"] = "/index3.html"})
-table.insert(h3, {[":scheme"] = "http"})
-table.insert(h3, {[":authority"] = "localhost:8080"})
-
-local fd = assert(io.open("fuck", "w"))
-local response_headers, s = http2.request("localhost", 8080, nil, h1)
+local fd = assert(io.open("request_out", "w"))
+local response_headers, s = http2.request("localhost", 8080, nil, headers)
 local body = s:get_body()
 
 for _, header in ipairs(response_headers) do
