261047c8b718a6c8ef470120fb2420c86de46a44
Murillo Rodrigues de Paula <murdrof@gmail.com>
2018-06-02 06:43:25 -0300

    test_request: sends a POST request using the new DATA frame encoding mechanism

--- test_request.lua
+++ test_request.lua
@@ -2,13 +2,13 @@ local http2 = require "http2"
 local copas = require "copas"
 
 local headers = {}
-table.insert(headers, {[":method"] = "GET"})
+table.insert(headers, {[":method"] = "POST"})
 table.insert(headers, {[":path"] = "/"})
 table.insert(headers, {[":scheme"] = "http"})
 table.insert(headers, {[":authority"] = "localhost:8080"})
 
 local fd = io.open("request_out", "w")
-local response_headers, s = http2.request("http://localhost:8080/")
+local response_headers, s = http2.request("http://localhost:8080/", "ff", headers)
 local body = s:get_body()
 s:encode_rst_stream(0x0)
 s:encode_goaway(s.connection.max_server_streamid, 0x0)
